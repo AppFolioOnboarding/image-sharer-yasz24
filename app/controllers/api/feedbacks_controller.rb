c5329d05bdb3b08995af5967e1d6b361531c0520
Yash Shetty <yash.shetty@appfolio.com>
2020-06-12 19:37:30 -0400

    ys - remove aunthentication skip

--- app/controllers/api/feedbacks_controller.rb
+++ app/controllers/api/feedbacks_controller.rb
@@ -1,7 +1,5 @@
 module Api
   class FeedbacksController < ApplicationController
-    skip_before_action :verify_authenticity_token
-
     def create
       @feedback = Feedback.new(feedback_params)
 
